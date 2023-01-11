aa37ef8311dbe4e0360a80c12261e90d5a55b2d7
Nemo <nemoyap@smu.edu.sg>
2023-01-11 13:51:55 +0800

    Converted LegalSS Quickstart documentation

---
+++ _build/html/_sources/index.rst.txt
@@ -0,0 +1,24 @@
+.. legalss-docs documentation master file, created by
+   sphinx-quickstart on Wed Jan 11 11:50:04 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to legalss-docs's documentation!
+========================================
+
+.. toctree::
+   :maxdepth: 4
+   :caption: Contents:
+
+   docs/legalss-executiveSummary.rst
+   docs/legalss-setup.rst
+   docs/legalss-definitionsAndDeclarations.rst
+   docs/legalss-examples.rst
+   docs/legalss-documentationFutureWork.rst
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ conf.py
@@ -0,0 +1,30 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'LegalSS'
+copyright = '2023, Meng Weng Wong'
+author = 'Meng Weng Wong'
+release = '0.9'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx_rtd_theme'
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
